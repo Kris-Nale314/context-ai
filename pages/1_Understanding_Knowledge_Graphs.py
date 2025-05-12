c34c802731fbff3a5f2a05bfbb615442e43a0193
Kris-Nale314 <kris.nale@gmail.com>
2025-05-12 12:23:45 -0400

    Enhanced KG basics overview

--- pages/1_Understanding_Knowledge_Graphs.py
+++ pages/1_Understanding_Knowledge_Graphs.py
@@ -18,7 +18,7 @@ if show_slide_at_top:
     
     st.markdown("""
     <div style="text-align: center;">
-        <img src="https://raw.githubusercontent.com/Kris-Nale314/context-ai/main/images/kgBasics.gif" width="1000">
+        <img src="https://raw.githubusercontent.com/Kris-Nale314/context-ai/main/images/kgBasics2.gif" width="1000">
     </div>
     """, unsafe_allow_html=True)
 
